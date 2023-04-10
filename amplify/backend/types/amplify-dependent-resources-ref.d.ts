d7a7fe93814a9aa3148f148fe8995df4c25729c6
EC2 Default User <ec2-user@ip-172-31-16-165.eu-central-1.compute.internal>
2023-04-10 11:05:21 +0000

    Added graphQL and Storage

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,26 @@
+export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "fullreactapp": {
+      "GraphQLAPIEndpointOutput": "string",
+      "GraphQLAPIIdOutput": "string",
+      "GraphQLAPIKeyOutput": "string"
+    }
+  },
+  "auth": {
+    "fullreactappe56aaed8": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  },
+  "storage": {
+    "s3e409a98c": {
+      "BucketName": "string",
+      "Region": "string"
+    }
+  }
+}
